84270be626687a1a39a212ee4bffc1b49e2a98e4
Dheeraj <dharundheeraj2007@gmail.com>
2026-07-14 09:16:49 +0530

    Time: 447 ms (11.55%), Space: 52.4 MB (26.85%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int till=0;
+        int n=nums.size()-1;
+        if(nums.size()==1){
+            return true;
+        }
+        for(int i=0;i<n;i++){
+            till=max(till,nums[i]+i);
+            cout<<till<<endl;
+            if(i>=till)return false;
+            if(till>=n){
+                return true;
+            }
+            //till=0;
+        }
+        return false;
+    }
+};
