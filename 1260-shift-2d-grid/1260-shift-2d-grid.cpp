9ad653447dd30cd5ebfc93cec8aa78ae2f3f83f6
Dheeraj <dharundheeraj2007@gmail.com>
2026-07-20 22:58:12 +0530

    Time: 2 ms (49.52%), Space: 19.3 MB (14.15%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.cpp
+++ 1260-shift-2d-grid/1260-shift-2d-grid.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    vector<vector<int>> shiftGrid(vector<vector<int>>& grid,int k) {
-        int m=grid.size(),n=grid[0].size();
-        vector<int> arr;
-
-        for(int i=0;i<m;i++)
-            for(int j=0;j<n;j++)
-                arr.push_back(grid[i][j]);
-
-        k%=arr.size();
-        rotate(arr.begin(),arr.end()-k,arr.end());
-
-        vector<vector<int>> ans(m,vector<int>(n));
+    vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
+        vector<int> tmp;
+        int m=grid.size();
+        int n=grid[0].size();
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                tmp.push_back(grid[i][j]);
+            }
+        }
+        k%=tmp.size();
+        rotate(tmp.begin(),tmp.end()-k,tmp.end());
         int idx=0;
-
-        for(int i=0;i<m;i++)
-            for(int j=0;j<n;j++)
-                ans[i][j]=arr[idx++];
-
+        vector<vector<int>> ans(m,vector<int>(n));
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                ans[i][j]=tmp[idx++];
+            }
+        }
         return ans;
     }
 };
