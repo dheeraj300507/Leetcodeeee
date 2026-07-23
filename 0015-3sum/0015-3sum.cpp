5b1c85ffe748e3b7b93b0d523bf60eefaf10c8a6
Dheeraj <dharundheeraj2007@gmail.com>
2026-07-23 10:44:15 +0530

    Time: 60 ms (18.38%), Space: 29 MB (73.56%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,34 +1,36 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> res;
-        sort(nums.begin(), nums.end());
-
-        for (int i = 0; i < nums.size(); i++) {
-            if (i > 0 && nums[i] == nums[i-1]) {
-                continue;
-            }
-            
-            int j = i + 1;
-            int k = nums.size() - 1;
-
-            while (j < k) {
-                int total = nums[i] + nums[j] + nums[k];
-
-                if (total > 0) {
-                    k--;
-                } else if (total < 0) {
-                    j++;
-                } else {
-                    res.push_back({nums[i], nums[j], nums[k]});
-                    j++;
-
-                    while (nums[j] == nums[j-1] && j < k) {
-                        j++;
+        //-4,-1,-1,0,1,2
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> ans;
+        int sum=0;
+        for(int i=0;i<nums.size();i++){
+            int l=i+1;
+            int r=nums.size()-1;
+            if(i>0 && nums[i]==nums[i-1])continue;
+            while(l<r){
+                sum=nums[i]+nums[l]+nums[r];
+                if(sum<0){
+                    l++;
+                    while(l<r && nums[l]==nums[l-1])l++;
+                }
+                else if(sum>0){
+                    r--;
+                    while(l<r && nums[r]==nums[r+1])r--;
+                }
+                else{
+                    ans.push_back({nums[i],nums[l],nums[r]});
+                    l++;
+                    r--;
+                    while(l<r && nums[l]==nums[l-1] && nums[r]==nums[r+1]){
+                        l++;
+                        r--;
                     }
                 }
             }
+        
         }
-        return res;        
+        return ans;
     }
 };
