ed9e699565b183464caf169b03a374b5015f3490
Dheeraj <dharundheeraj2007@gmail.com>
2026-07-28 11:29:43 +0530

    Time: 0 ms (100%), Space: 19.5 MB (72.13%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,49 +1,51 @@
 class Solution {
 public:
-    vector<int> parent,rank;
-
-    int find(int x) {
-        if (parent[x] == x)
-            return x;
-        return parent[x] = find(parent[x]);
+    int find(vector<int>& leader,int node){
+        if(leader[node]==node){
+            return node;
+        }
+        return leader[node]=find(leader,leader[node]);
+    }
+    void join(vector<int>& leader,int left,int right){
+        left=find(leader,left);
+        right=find(leader,right);
+        if(left!=right){
+            leader[left]=right;
+        }
     }
 
-    void Union(int x, int y) {
-        int px = find(x);
-        int py = find(y);
 
-        if (px == py)
-            return;
+    int findCircleNum(vector<vector<int>>& grid) {
+        int N=grid[0].size();
+        vector<int> leader(N+1);
 
-        if (rank[px] < rank[py])
-            swap(px, py);
-        parent[py] = px;
-        if (rank[px] == rank[py])
-            rank[px]++;
-    }
+        for(int i=0;i<=N;i++){
+            leader[i]=i;
+        }
 
-    int findCircleNum(vector<vector<int>>& isConnected) {
-        int n = isConnected.size();
-
-        parent.resize(n);
-        rank.assign(n, 0);
-
-        iota(parent.begin(), parent.end(), 0);
-
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (isConnected[i][j])
-                    Union(i, j);
+        for(int i=0;i<N;i++){
+            for(int j=0;j<N;j++){
+                if(grid[i][j]==1){
+                    join(leader,i+1,j+1);
+                }
             }
         }
 
-        int ans = 0;
+        vector<int> gcnt(N+1,0);
 
-        for (int i = 0; i < n; i++) {
-            if (find(i) == i)
-                ans++;
+        for(int i=0;i<=N;i++){
+            int super=find(leader,i);
+            gcnt[super]++;
         }
 
-        return ans;
+        int groups=0;
+
+        for(int i=1;i<=N;i++){
+            if(gcnt[i]>0){
+                groups++;
+            }
+        }
+
+        return groups;
     }
 };
